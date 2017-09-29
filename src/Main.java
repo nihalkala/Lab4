4fb2ddd53e650392b25d80ef65b937ad95d9aace
nihalkala <nihalkala@ufl.edu>
2017-09-29 11:08:26 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class Main {
     public static void main(String args []){
         Scanner scn = new Scanner(System.in);
-        System.out.print("Hello world!");
+        System.out.print("Hello world! ");
     }
 }
